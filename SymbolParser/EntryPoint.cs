cbcc1a6bf97a76084007f81417819d2e4b9ee8ba
Liareth <liarethnwn@gmail.com>
2015-10-25 11:18:21 +0000

    Disabled exception handler in debug mode, so the debugger can catch exceptions.

--- SymbolParser/EntryPoint.cs
+++ SymbolParser/EntryPoint.cs
@@ -6,16 +6,20 @@ namespace SymbolParser
     {
         public static void Main(string[] args)
         {
+#if !DEBUG
             try
             {
+#endif
                 CommandLine.parseArgs(args);
                 var parser = new SymbolParser();
+#if !DEBUG
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Failed to parse the file because: " + ex.Message);
                 Console.Read();
             }
+#endif
         }
     }
 }
