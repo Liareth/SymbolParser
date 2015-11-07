b7abb9e5d0ec796e7c50a5967d1b5c905c85221d
Liareth <liarethnwn@gmail.com>
2015-11-07 16:34:55 +0000

    Use standard library sized typedefs on every platform now, not just Linux.

--- SymbolParser/CppType.cs
+++ SymbolParser/CppType.cs
@@ -6,7 +6,6 @@ namespace SymbolParser
 {
     public enum BuiltInCppTypes
     {
-        // Platform-dependant types.
         INT8,
         UNSIGNED_INT8,
         SIGNED_INT8,
@@ -19,30 +18,15 @@ namespace SymbolParser
         INT64,
         UNSIGNED_INT64,
         SIGNED_INT64,
-
-        // Standard types
-        BOOL,
         CHAR,
-        DOUBLE,
-        FLOAT,
-        INT,
-        LONG_DOUBLE,
-        LONG_INT,
-        LONG_LONG_INT,
-        SHORT_INT,
         SIGNED_CHAR,
-        SIGNED_INT,
-        SIGNED_LONG_INT,
-        SIGNED_LONG_LONG_INT,
-        SIGNED_SHORT_INT,
         UNSIGNED_CHAR,
-        UNSIGNED_INT,
-        UNSIGNED_LONG_INT,
-        UNSIGNED_LONG_LONG_INT,
-        UNSIGNED_SHORT_INT,
-        VOID,
         WCHAR_T,
-        FUNCTION
+        VOID,
+        BOOL,
+        FLOAT,
+        DOUBLE,
+        FUNCTION,
     };
 
     public class CppType
@@ -279,53 +263,27 @@ namespace SymbolParser
                     return "int64_t";
                 case BuiltInCppTypes.UNSIGNED_INT64:
                     return "uint64_t";
-                case BuiltInCppTypes.BOOL:
-                    return "bool";
                 case BuiltInCppTypes.CHAR:
                     return "char";
-                case BuiltInCppTypes.DOUBLE:
-                    return "double";
-                case BuiltInCppTypes.FLOAT:
-                    return "float";
-                case BuiltInCppTypes.INT:
-                    return "int";
-                case BuiltInCppTypes.LONG_DOUBLE:
-                    return "long double";
-                case BuiltInCppTypes.LONG_INT:
-                    return "long int";
-                case BuiltInCppTypes.LONG_LONG_INT:
-                    return "long long";
-                case BuiltInCppTypes.SHORT_INT:
-                    return "short int";
                 case BuiltInCppTypes.SIGNED_CHAR:
                     return "signed char";
-                case BuiltInCppTypes.SIGNED_INT:
-                    return "signed int";
-                case BuiltInCppTypes.SIGNED_LONG_INT:
-                    return "signed long int";
-                case BuiltInCppTypes.SIGNED_LONG_LONG_INT:
-                    return "signed long long int";
-                case BuiltInCppTypes.SIGNED_SHORT_INT:
-                    return "signed short int";
                 case BuiltInCppTypes.UNSIGNED_CHAR:
                     return "unsigned char";
-                case BuiltInCppTypes.UNSIGNED_INT:
-                    return "unsigned int";
-                case BuiltInCppTypes.UNSIGNED_LONG_INT:
-                    return "unsigned long int";
-                case BuiltInCppTypes.UNSIGNED_LONG_LONG_INT:
-                    return "unsigned long long int";
-                case BuiltInCppTypes.UNSIGNED_SHORT_INT:
-                    return "unsigned short int";
-                case BuiltInCppTypes.VOID:
-                    return "void";
                 case BuiltInCppTypes.WCHAR_T:
                     return "wchar_t";
+                case BuiltInCppTypes.VOID:
+                    return "void";
+                case BuiltInCppTypes.BOOL:
+                    return "bool";
+                case BuiltInCppTypes.FLOAT:
+                    return "float";
+                case BuiltInCppTypes.DOUBLE:
+                    return "double";
                 case BuiltInCppTypes.FUNCTION:
                     return "void";
             }
 
-            Debug.Assert(false, "Could not construct a string representation for type {type}!");
+            Debug.Assert(false, "Could not construct a string representation for type " + type.ToString());
             return null;
         }
 
@@ -348,109 +306,96 @@ namespace SymbolParser
                     return BuiltInCppTypes.SIGNED_INT8;
                 case "__int16":
                 case "int16_t":
+                case "short":
+                case "short int":
+                case "short^int":
                     return BuiltInCppTypes.INT16;
                 case "unsigned __int16":
                 case "unsigned^__int16":
                 case "uint16_t":
-                    return BuiltInCppTypes.UNSIGNED_INT16;
-                case "signed __int16":
-                case "signed^__int16":
-                    return BuiltInCppTypes.SIGNED_INT16;
-                case "__int32":
-                case "int32_t":
-                    return BuiltInCppTypes.INT32;
-                case "unsigned __int32":
-                case "unsigned^__int32":
-                case "uint32_t":
-                    return BuiltInCppTypes.UNSIGNED_INT32;
-                case "signed __int32":
-                case "signed^__int32":
-                    return BuiltInCppTypes.SIGNED_INT32;
-                case "__int64":
-                case "int64_t":
-                    return BuiltInCppTypes.INT64;
-                case "unsigned __int64":
-                case "unsigned^__int64":
-                case "uint64_t":
-                    return BuiltInCppTypes.UNSIGNED_INT64;
-                case "signed __int64":
-                case "signed^__int64":
-                    return BuiltInCppTypes.SIGNED_INT64;
-                case "bool":
-                    return BuiltInCppTypes.BOOL;
-                case "char":
-                    return BuiltInCppTypes.CHAR;
-                case "double":
-                    return BuiltInCppTypes.DOUBLE;
-                case "float":
-                    return BuiltInCppTypes.FLOAT;
-                case "int":
-                    return BuiltInCppTypes.INT;
-                case "long double":
-                    return BuiltInCppTypes.LONG_DOUBLE;
-                case "long":
-                case "long int":
-                case "long^int":
-                    return BuiltInCppTypes.LONG_INT;
-                case "long long":
-                case "long long int":
-                case "long^long":
-                case "long^long^int":
-                    return BuiltInCppTypes.LONG_LONG_INT;
-                case "short":
-                case "short int":
-                case "short^int":
-                    return BuiltInCppTypes.SHORT_INT;
-                case "signed char":
-                case "signed^char":
-                    return BuiltInCppTypes.SIGNED_CHAR;
-                case "signed int":
-                case "signed^int":
-                    return BuiltInCppTypes.SIGNED_INT;
-                case "signed long":
-                case "signed long int":
-                case "signed^long":
-                case "signed^long^int":
-                    return BuiltInCppTypes.SIGNED_LONG_INT;
-                case "signed long long":
-                case "signed long long int":
-                case "signed^long^long":
-                case "signed^long^long^int":
-                    return BuiltInCppTypes.SIGNED_LONG_LONG_INT;
-                case "signed short":
-                case "signed short int":
-                case "signed^short":
-                case "signed^short^int":
-                    return BuiltInCppTypes.SIGNED_SHORT_INT;
-                case "unsigned char":
-                case "unsigned^char":
-                    return BuiltInCppTypes.UNSIGNED_CHAR;
-                case "unsigned":
-                case "unsigned int":
-                case "unsigned^int":
-                    return BuiltInCppTypes.UNSIGNED_INT;
-                case "unsigned long":
-                case "unsigned long int":
-                case "unsigned^long":
-                case "unsigned^long^int":
-                    return BuiltInCppTypes.UNSIGNED_LONG_INT;
-                case "unsigned long long":
-                case "unsigned long long int":
-                case "unsigned^long^long":
-                case "unsigned^long^long^int":
-                    return BuiltInCppTypes.UNSIGNED_LONG_LONG_INT;
                 case "unsigned short":
                 case "unsigned short int":
                 case "unsigned^short":
                 case "unsigned^short^int":
-                    return BuiltInCppTypes.UNSIGNED_SHORT_INT;
-                case "void":
-                    return BuiltInCppTypes.VOID;
+                    return BuiltInCppTypes.UNSIGNED_INT16;
+                case "signed __int16":
+                case "signed^__int16":
+                case "signed short":
+                case "signed short int":
+                case "signed^short":
+                case "signed^short^int":
+                    return BuiltInCppTypes.SIGNED_INT16;
+                case "__int32":
+                case "int32_t":
+                case "int":
+                case "long":
+                case "long int":
+                case "long^int":
+                    return BuiltInCppTypes.INT32;
+                case "unsigned __int32":
+                case "unsigned^__int32":
+                case "uint32_t":
+                case "unsigned":
+                case "unsigned int":
+                case "unsigned^int":
+                case "unsigned long":
+                case "unsigned long int":
+                case "unsigned^long":
+                case "unsigned^long^int":
+                    return BuiltInCppTypes.UNSIGNED_INT32;
+                case "signed __int32":
+                case "signed^__int32":
+                case "signed int":
+                case "signed^int":
+                case "signed long":
+                case "signed long int":
+                case "signed^long":
+                case "signed^long^int":
+                    return BuiltInCppTypes.SIGNED_INT32;
+                case "__int64":
+                case "int64_t":
+                case "long long":
+                case "long long int":
+                case "long^long":
+                case "long^long^int":
+                    return BuiltInCppTypes.INT64;
+                case "unsigned __int64":
+                case "unsigned^__int64":
+                case "uint64_t":
+                case "unsigned long long":
+                case "unsigned long long int":
+                case "unsigned^long^long":
+                case "unsigned^long^long^int":
+                    return BuiltInCppTypes.UNSIGNED_INT64;
+                case "signed __int64":
+                case "signed^__int64":
+                case "signed long long":
+                case "signed long long int":
+                case "signed^long^long":
+                case "signed^long^long^int":
+                    return BuiltInCppTypes.SIGNED_INT64;
+                case "char":
+                    return BuiltInCppTypes.CHAR;
+                case "unsigned char":
+                case "unsigned^char":
+                    return BuiltInCppTypes.UNSIGNED_CHAR;
+                case "signed char":
+                case "signed^char":
+                    return BuiltInCppTypes.SIGNED_CHAR;
                 case "wchar_t":
                     return BuiltInCppTypes.WCHAR_T;
-                default:
-                    return null;
+                case "void":
+                    return BuiltInCppTypes.VOID;
+                case "bool":
+                    return BuiltInCppTypes.BOOL;
+                case "double":
+                    return BuiltInCppTypes.DOUBLE;
+                case "float":
+                    return BuiltInCppTypes.FLOAT;
             }
+
+            Debug.Assert(false, "Could not construct an enum representation from type " + type);
+            return null;
         }
 
         private string toStringRepresentation()
